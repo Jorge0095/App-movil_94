b86c146a25987b83c97ffaa3bbaeeb2229baf0a3
Jorge0095 <2022030095@upsin.edu.mx>
2025-05-19 16:28:48 -0700

    Se logro remplazar, como actividad inicial, de 'main_activity' a 'menu_activity', permitiendo una forma de navegar por las distintas aplicaciones dentro de la app; se incluye un adelanto a la actividad secundaria de 'IMC'.

--- app/src/main/java/com/example/ultimocuatri/IMC.kt
+++ app/src/main/java/com/example/ultimocuatri/IMC.kt
@@ -1,30 +1,28 @@
 package com.example.ultimocuatri
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    // Declaración de Objetos
-    private lateinit var lblSaludo : TextView
-    private lateinit var txtNombre : EditText
-    private lateinit var btnSaludar : Button
+class IMC : AppCompatActivity() {
+    private lateinit var txtAltura : EditText
+    private lateinit var txtPeso : EditText
+    private lateinit var lblResultado : TextView
+    private lateinit var btnCalcular : Button
     private lateinit var btnLimpiar : Button
     private lateinit var btnCerrar : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_imc)
 
         iniciarComponentes()
         eventosBotones()
@@ -37,42 +35,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun iniciarComponentes(){
-
-        // Relacionar los componentes del Layout con los objetos de Kotlin.
-        lblSaludo = findViewById(R.id.lblSaludo)
-        txtNombre = findViewById(R.id.txtNombre)
-        btnSaludar = findViewById(R.id.btnSaludar)
+        txtAltura = findViewById(R.id.txtAltura)
+        txtPeso = findViewById(R.id.txtPeso)
+        lblResultado = findViewById(R.id.lblResultado)
+        btnCalcular = findViewById(R.id.btnCalcular)
         btnLimpiar = findViewById(R.id.btnLimpiar)
         btnCerrar = findViewById(R.id.btnCerrar)
-
     }
 
-
     fun eventosBotones(){
-        // Codificar el evento del botón Saludar
-        btnSaludar.setOnClickListener(View.OnClickListener {
-            // Declarar variable
-            var strNombre : String = ""
-            if (txtNombre.text.toString().contentEquals("")){
-                Toast.makeText(applicationContext,
-                    "Falto capturar el Nombre", Toast.LENGTH_SHORT).show()
-            } else {
-                strNombre = " Hola " + txtNombre.text.toString() + " Cómo estás ?"
-                // Asignar nombre a Edit Text
-                lblSaludo.text = strNombre
-            }
-        }) // Fin btnSaludar.
-        btnLimpiar.setOnClickListener(View.OnClickListener {
-            txtNombre.setText("")
-            lblSaludo.setText("")
-
-        }) // Fin btnLimpiar.
-
-
-
-
-
-        btnCerrar.setOnClickListener(View.OnClickListener {
+        btnCalcular.setOnClickListener(){
+            // iniciar variables
+            var altura : Double = 0.0
+            var peso : Double = 0.0
+            var imc : Double = 0.0
+            // Asignar valores
+            altura = txtAltura.text.toString().toDouble()
+            peso = txtPeso.text.toString().toDouble()
+            imc = peso / (altura * altura)
+            // Mostrar resultado
+            lblResultado.text = "Tu IMC es de: " + imc.toString()
+        }
+        btnLimpiar.setOnClickListener(){
+            txtAltura.setText("")
+            txtPeso.setText("")
+            lblResultado.text = ""
+        }
+        btnCerrar.setOnClickListener(){
             val builder = AlertDialog.Builder(this)
             builder.setTitle("App Hola ")
             builder.setMessage("¿Desea Cerrar la Aplicación?")
@@ -92,10 +81,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             builder.show()
-
-        }) // Fin btnCerrar.
-
-    } // Cierra función eventosBotones.
-
-
-} // Cierra la clase.
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ultimocuatri/MenuActivity.kt
@@ -0,0 +1,55 @@
+package com.example.ultimocuatri
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var crvHola : CardView
+    private lateinit var crvImc : CardView
+    private lateinit var crvConvertir : CardView
+    private lateinit var crvMonedas : CardView
+    private lateinit var crvCotizacion : CardView
+    private lateinit var crvSpiner : CardView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        // Llamar funciones
+        iniciarComponentes()
+        eventosBotones()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun iniciarComponentes(){
+        crvHola = findViewById(R.id.crvHola) as CardView
+        crvImc = findViewById(R.id.crvImc) as CardView
+        crvConvertir = findViewById(R.id.crvConvertir) as CardView
+        crvMonedas = findViewById(R.id.crvMonedas) as CardView
+        crvCotizacion = findViewById(R.id.crvCotizacion) as CardView
+        crvSpiner = findViewById(R.id.crvSpiner) as CardView
+    }
+    fun eventosBotones(){
+        crvHola.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+        crvImc.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, IMC::class.java)
+            startActivity(intent)
+        })
+
+    }
+}
